aa971b3b37f95a75c7edf23d30eb0749326885f5
ARCHANA-MURALI <117839833+ARCHANA-MURALI@users.noreply.github.com>
2022-12-28 23:36:17 +0530

    Update pyramig.py

--- pyramig.py
+++ pyramig.py
@@ -1,6 +1,7 @@
 rows=int(input("Enter number of rows and eg(N=4)"))
+Step=int(input("Enter step no:eg(step=1"))
 
-for i in range(1,rows+1):
-    for j in range (1,i+1):
+for i in range(1,rows+1,step):
+    for j in range (1,i+1,step):
         print(i*j,end="  ")
     print("\n")
